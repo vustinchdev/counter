0269f3fa57b1c4b1daeb2bb4d3d0b4bab05b8594
vustinchdev <vustinchdev@gmail.com>
2023-10-19 16:53:36 +0300

    added FlexWrapper

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
     colors: {
-        primaryColor: '#1c313f',
-        secondaryColor: '#ff0000',
-        accent: '#73baec',
+        primaryColor: "#1c313f",
+        secondaryColor: "#ff0000",
+        accent: "#73baec",
     }
 }
